5055d7609cbbcedd92bcbd73e6822723248b7162
fabian lopez <f.ls9323@gmail.com>
2025-04-04 16:24:22 -0500

    actualizacion de cotizaciones y filtrado de imagnes y all de imagenes

--- routes/routes.js
+++ routes/routes.js
@@ -8,12 +8,14 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 router
     
-    .post('/upload', upload.array('image',10), controllers.SubirImagenes)
+    .post('/upload', upload.array('image',10), controllers.crearProductos)
     .post('/cotizacion', upload.array('image',10), controllers.subirCotizaciones)
     .post('/sabor', controllers.registroSabores)
     .post('/porciones', controllers.registroPorcioes)
     .get('/cotizacionuser/:userid', controllers.cotizacionUser)
     .get('/cotizacionall', controllers.cotizacionAll)
+    .get('/getimagenes', controllers.allmagenes)
+    .get('/getfiltraImage', controllers.filtraImage)
     .patch('/modificarcoti/:idcotizacion', controllers.cotizacionModificar)
     
 
